8a12978fdc0269ba30ce7cb924430b5fa2f4f112
BrunoRabbit <72535437+BrunoRabbit@users.noreply.github.com>
2020-10-08 00:00:29 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,33 @@
+//Bruno Coelho Alves
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+    Scanner leia = new Scanner(System.in);
+    
+    double num;
+
+    System.out.print("Insira um valor: ");
+    num = leia.nextDouble();
+
+    boolean retornoPositivo = isPositive(num);
+    boolean retornoNegativo = isNegative(num);
+    boolean retornoZero = isZero(num);
+    if(retornoPositivo == true){
+        System.out.println("O valor é positivo!!");
+    }else if(retornoNegativo == true){
+        System.out.println("O valor é negativo!!");
+    }else if(retornoZero){
+        System.out.println("O valor é Zero!!");
+    }
+    leia.close();
+    }
+    static boolean isPositive(double num){
+        return num >= 1;
+    }
+    static boolean isNegative(double num){
+        return num < 0;
+    }
+    static boolean isZero(double num){
+        return num == 0;
+    }
+}
