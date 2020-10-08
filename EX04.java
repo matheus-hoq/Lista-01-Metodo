e2458e2785f528b5b08920a6d5a28a1b96c2c8e4
BrunoRabbit <72535437+BrunoRabbit@users.noreply.github.com>
2020-10-08 15:32:48 -0300

    Add files via upload

---
+++ EX04.java
@@ -0,0 +1,28 @@
+//Bruno Coelho Alves
+import java.util.Scanner;
+public class EX04 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        double a;
+        double b;
+        double c;
+        double delta;
+
+        System.out.print("Insira o valor: ");
+        a = leia.nextDouble();
+
+        System.out.print("Insira o valor: ");
+        b = leia.nextDouble();
+
+        System.out.print("Insira o valor: ");
+        c = leia.nextDouble();
+        
+        delta = (b*b) - (4 * a * c);
+       
+        leia.close();
+
+        System.out.println("Valor de delta: " + delta);
+    }
+    
+   
+}
