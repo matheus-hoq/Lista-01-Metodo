e7830b4012a67acbc3963071add34610ba63047b
BrunoRabbit <72535437+BrunoRabbit@users.noreply.github.com>
2020-10-08 00:01:10 -0300

    Update and rename App.java to EX03.java

--- EX03.java
+++ EX03.java
@@ -1,6 +1,6 @@
 //Bruno Coelho Alves
 import java.util.Scanner;
-public class App {
+public class EX03 {
     public static void main(String[] args) {
     Scanner leia = new Scanner(System.in);
     
